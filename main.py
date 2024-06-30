f5f508e6b8c3d6c257e628519bed402d9b9445d4
AshokkumarMCA <157869054+AshokkumarMCA@users.noreply.github.com>
2024-06-30 11:34:00 +0530

    kovil website

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template,request,Response
+app=Flask(__name__)
+app.secret_key="vilkvm"
+@app.route('/',methods=["POST","GET"])
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
